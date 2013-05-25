a3bea522a956416164ca2229712bf6b389bf6baf
Jaromir Nyklicek <nyklicek@gmail.com>
2013-05-25 12:38:30 +0200

    Minor changes in flash messaging.

--- public/admin/services/ErrorHandler.js
+++ public/admin/services/ErrorHandler.js
@@ -6,17 +6,15 @@ aidbotAdminApp.factory('errorHandler', function ($q, $flash, $location) {
       // everything is ok, don't do anything
       return response;
     }, function (response) {
-      var message = 'Error occured during the communication with webservice. Try it later, please.';
-
       if (response.status == 401) {
-        $flash.notify('alert', message);
+        $flash.notify('alert', 'You are not allowd to manipulate with this resource. Please log in.');
         $location.path('/');
       }
 
+      var message = 'Error occured during the communication with webservice. Try it later, please.';
       if (response.data.message !== undefined) {
         message = response.data.message;
       }
-
       $flash.notify('alert', message);
       return $q.reject(response);
     });
